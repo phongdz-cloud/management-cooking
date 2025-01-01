7662adb34d09a7b82724478311aaeae2cca7e84c
phonghh <longtam20015@gmail.com>
2025-01-01 11:46:07 +0700

    Tạo Login Route Handler

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,40 @@
+import authApiRequest from "@/app/apiRequests/auth";
+import { LoginBodyType } from "@/schemaValidations/auth.schema";
+import { cookies } from "next/headers";
+import jwt from "jsonwebtoken";
+import { HttpError } from "@/lib/http";
+export async function POST(request: Request) {
+  // trả về request gửi lên
+  const body = (await request.json()) as LoginBodyType;
+
+  const cookieStore = await cookies();
+
+  try {
+    const { payload } = await authApiRequest.sLogin(body);
+    const { accessToken, refreshToken } = payload.data;
+    const decodedAccessToken = jwt.decode(accessToken) as { exp: number };
+    const decodedRefreshToken = jwt.decode(refreshToken) as { exp: number };
+    // Set cookie accessToken, refreshToken
+    cookieStore.set("accessToken", accessToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedAccessToken.exp * 1000,
+    });
+    cookieStore.set("refreshToken", refreshToken, {
+      path: "/",
+      httpOnly: true,
+      sameSite: "lax",
+      secure: true,
+      expires: decodedRefreshToken.exp * 1000,
+    });
+    return Response.json(payload);
+  } catch (error) {
+    if (error instanceof HttpError) {
+      return Response.json(error.payload, { status: error.status });
+    } else {
+      return Response.json({ message: "Có lỗi xảy ra" }, { status: 500 });
+    }
+  }
+}
---
+++ src/app/apiRequests/auth.ts
@@ -0,0 +1,13 @@
+import http from "@/lib/http";
+import { LoginBodyType, LoginResType } from "@/schemaValidations/auth.schema";
+
+const authApiRequest = {
+  sLogin: async (body: LoginBodyType) =>
+    http.post<LoginResType>("/auth/login", body),
+  login: async (body: LoginBodyType) =>
+    http.post<LoginResType>("/api/auth/login", body, {
+      baseUrl: "",
+    }),
+};
+
+export default authApiRequest;
