aeaf3c0c6c2ce750c05a7f712c0f42433c137c12
phonghh <longtam20015@gmail.com>
2025-01-07 21:25:38 +0700

    Refactor QRCodeTable component to enhance canvas rendering and add text labels

--- src/components/qrcode-table.tsx
+++ src/components/qrcode-table.tsx
@@ -13,21 +13,46 @@ const QRCodeTable = ({
   width?: number
 }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null)
+
   useEffect(() => {
-    if (canvasRef.current) {
-      QRCode.toCanvas(
-        canvasRef.current,
-        getTableLink({
-          token,
-          tableNumber,
-        }),
-        (error) => {
-          if (error) {
-            console.error(error)
-          }
+    if (!canvasRef.current) return
+
+    const canvas = canvasRef.current
+    canvas.height = width + 70
+    canvas.width = width
+    const canvasContext = canvas.getContext('2d')
+    if (!canvasContext) return
+    canvasContext.fillStyle = '#fff'
+    canvasContext.fillRect(0, 0, canvas.width, canvas.height)
+    canvasContext.font = '20px Arial'
+    canvasContext.textAlign = 'center'
+    canvasContext.fillStyle = '#000'
+    canvasContext.fillText(
+      `Bàn số ${tableNumber}`,
+      canvas.width / 2,
+      canvas.width + 20
+    )
+    canvasContext.fillText(
+      `Quét mã QR để gọi món`,
+      canvas.width / 2,
+      canvas.width + 50
+    )
+
+    const virtalCanvas = document.createElement('canvas')
+    QRCode.toCanvas(
+      virtalCanvas,
+      getTableLink({
+        token,
+        tableNumber,
+      }),
+      (error) => {
+        if (error) {
+          console.log(error)
+        } else {
+          canvasContext.drawImage(virtalCanvas, 0, 0, width, width)
         }
-      )
-    }
+      }
+    )
   }, [token, tableNumber, canvasRef.current])
   return <canvas ref={canvasRef} />
 }
